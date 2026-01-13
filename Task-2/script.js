5405941d12ed2f8e96e63eb2910d17e8532efaa3
Kp9460 <parmarks800@gmail.com>
2026-01-13 17:37:40 +0530

    Add fullname,captain,trophies

--- Task-2/script.js
+++ Task-2/script.js
@@ -1,51 +1,84 @@
-
 var arr = [
     {
         team: 'RCB',
         primary: 'red',
-        secondary: 'darkgoldenred'
+        secondary: 'darkgoldenred',
+        fullName: 'Royal Challengers Bengaluru',
+        trophies: '🏆',
+        captain: 'Rajat Patidar'
     },{
         team: 'MI',
         primary: 'blue',
-        secondary: 'white'
+        secondary: 'white',
+        fullName: 'Mumbai Indians',
+        trophies: '🏆🏆🏆🏆🏆',
+        captain: 'Hardik Pandya'
     },{
         team: 'CSK',
         primary: 'yellow',
-        secondary: 'white'
+        secondary: 'white',
+        fullName: 'Chennai Super Kings',
+        trophies: '🏆🏆🏆🏆🏆',
+        captain: 'MSD'
     },{
         team: 'GT',
         primary: '#0b0b33f8',
-        secondary: 'darkgoldenrod'
+        secondary: 'darkgoldenrod',
+        fullName: 'Gujrat Titans',
+        trophies: '🏆',
+        captain: 'Shubman Gill'
     },{
         team: 'LSG',
         primary: 'turquoise',
-        secondary: 'white'
+        secondary: 'white',
+        fullName: 'Lucknow Super Giants',
+        trophies: '❌',
+        captain: 'Rishabh Pant'
     },{
         team: 'KKR',
         primary: 'purple',
-        secondary: 'darkgoldenrod'
+        secondary: 'darkgoldenrod',
+        fullName: 'Kolkata Knight Riders',
+        trophies: '🏆🏆🏆',
+        captain: 'Ajinkya Rahane'
     },{
         team: 'RR',
         primary: 'deeppink',
-        secondary: 'white'
+        secondary: 'white',
+        fullName: 'Rajasthan Royals',
+        trophies: '🏆',
+        captain: 'Riyan Prag'
     },{
         team: 'SRH',
         primary: 'orangered',
-        secondary: 'darkgoldenrod'
+        secondary: 'darkgoldenrod',
+        fullName: 'Sunrisers Hyderabad',
+        trophies: '🏆',
+        captain: 'Pat Cummins'
     },{
         team: 'PBKS',
         primary: 'crimson',
-        secondary: 'silver'
+        secondary: 'silver',
+        fullName: 'Punjab Kings',
+        trophies: '❌',
+        captain: 'Shreyas Iyer'
     },{
         team: 'DC',
         primary: 'blue',
-        secondary: 'snow'
+        secondary: 'snow',
+        fullName: 'Delhi Capitals',
+        trophies: '❌',
+        captain: 'Axar Patel'
     }
 ]
 
 var btn = document.querySelector("button");
 var h1 = document.querySelector("h1");
 var main = document.querySelector("main");
+var fullName = document.querySelector("#fullname");
+var captain = document.querySelector("#captain");
+var trophy = document.querySelector("#trophies");
+
 
 btn.addEventListener('click',function(){
 
@@ -54,5 +87,7 @@ btn.addEventListener('click',function(){
     h1.innerHTML = winner.team
     h1.style.backgroundColor = winner.secondary
     main.style.backgroundColor = winner.primary
-
-})
+    fullName.innerHTML = winner.fullName
+    captain.innerHTML = winner.captain
+    trophy.innerHTML = winner.trophies
+});
