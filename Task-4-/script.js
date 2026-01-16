cefc60cba7d19ad8205e8061e9ebe9d6fdb780be
Kp9460 <parmarks800@gmail.com>
2026-01-16 22:37:46 +0530

    timeout

---
+++ Task-4-/script.js
@@ -0,0 +1,30 @@
+let grow = 0
+let btn = document.querySelector("button");
+let h2 = document.querySelector("h2");
+let inner = document.querySelector(".inner");
+
+btn.addEventListener('click',()=> {
+
+    btn.style.pointerEvents = 'none'
+
+    let num = 50 + Math.floor(Math.random()*50);
+
+    console.log('Your file will be downloaded', num/10,'seconds');
+
+    let int = setInterval(()=> {
+        grow++;
+
+        h2.innerHTML = grow+'%'
+        inner.style.width = grow+'%'
+
+    },num);
+
+    setTimeout(()=> {
+        clearInterval(int);
+        btn.innerHTML = 'Dowloaded'
+        btn.style.opacity = '0.5'
+    }, num*100);
+
+    
+
+});
